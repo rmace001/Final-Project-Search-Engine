208eaee45ae3ef6fbbcdcc20e4a718c9c547b919
shiyao feng <32398815+Windbless@users.noreply.github.com>
2019-03-09 16:22:07 -0800

    update

--- searchfile.py
+++ searchfile.py
@@ -1,8 +1,63 @@
+import string
 import os
-directory = 'data/level2/'
+import glob
+import numpy as np
+import pandas as pd
+import math
+#For processing file in particular format based off of <doc>/<DOCNO>
+from bs4 import BeautifulSoup
+#For stemming
+from nltk.stem import PorterStemmer
+#For use of tokenizing to be applied with stemming (NOT USED)
+from nltk.tokenize import word_tokenize
+import operator
+import urllib.request
+import re
 
-for filename in os.listdir(directory):
-    print(filename)
-    file = open(directory + filename, "r")
-    print(file.readline())
+def htmlParse(newdirectory, filename, level, page_number):
+
+
+    #Start the soup
+    soup = BeautifulSoup(open(newdirectory+ "/"+ filename),'lxml')
+
+    #Get URL
+    file_open =  open(newdirectory+ "/" + filename, 'r') 
+    web_url = file_open.readline()
+
+    #Get title works correctly
+    try:
+        web_title = soup.find('title').get_text()
+    except:
+        web_title = ""
+    
+    #Get body works correctly
+    tempBody = []
+    for sentences in soup.find_all('p'):
+        tempBody.append(sentences.getText())
+
+    #Pop the source URL before converting to string
+    tempBody.pop(0)
+    body = ''.join(str(word) for word in tempBody)
+    body = body.replace("\n", "")
+    body = re.sub(' +', ' ', body)
+
+directory = 'data_1/'
+count  = 0
+for i in range(1, len(os.listdir(directory))-10):
+    newdirectory =  directory + "level" +str(i) 
+    for filename in os.listdir(newdirectory):
+        count += 1
+        htmlParse(newdirectory, filename, i, count)
+
+
+#getFiles()
+
+
+
+
+
+
+
+
+
 
