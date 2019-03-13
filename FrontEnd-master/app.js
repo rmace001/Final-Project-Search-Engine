ceb6d9da08cba898607b26957cfa3ef17b32db73
Rogelio Macedo <rmace001@ucr.edu>
2019-03-13 02:02:39 +0000

    Try this instead

--- FrontEnd-master/app.js
+++ FrontEnd-master/app.js
@@ -1,38 +1,23 @@
-var d3 = require("d3"),
-    cloud = require("../");
+var Canvas = require("canvas");
 
-var layout = cloud()
-    .size([500, 500])
-    .words([
-      "Hello", "world", "normally", "you", "want", "more", "words",
-      "than", "this"].map(function(d) {
-      return {text: d, size: 10 + Math.random() * 90, test: "haha"};
-    }))
+var cloud = require("../");
+
+var words = ["Hello", "world", "normally", "you", "want", "more", "words", "than", "this"]
+    .map(function(d) {
+      return {text: d, size: 10 + Math.random() * 90};
+    });
+
+cloud().size([960, 500])
+    .canvas(function() { return new Canvas(1, 1); })
+    .words(words)
     .padding(5)
     .rotate(function() { return ~~(Math.random() * 2) * 90; })
     .font("Impact")
     .fontSize(function(d) { return d.size; })
-    .on("end", draw);
+    .on("end", end)
+    .start();
 
-layout.start();
-
-function draw(words) {
-  d3.select("body").append("svg")
-      .attr("width", layout.size()[0])
-      .attr("height", layout.size()[1])
-    .append("g")
-      .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
-    .selectAll("text")
-      .data(words)
-    .enter().append("text")
-      .style("font-size", function(d) { return d.size + "px"; })
-      .style("font-family", "Impact")
-      .attr("text-anchor", "middle")
-      .attr("transform", function(d) {
-        return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-      })
-      .text(function(d) { return d.text; });
-}
+function end(words) { console.log(JSON.stringify(words)); } 
 
 
 
